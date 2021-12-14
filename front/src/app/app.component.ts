aa3c115cb51bc0dde5abcf4899ed3bffe9880a26
Lorenzo Manoeuvre <lorenzo.manoeuvre@epitech.eu>
2021-12-14 10:41:42 +0100

    trying monaco-editor

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {
+  MonacoEditorComponent,
+  MonacoEditorConstructionOptions,
+  MonacoEditorLoaderService,
+  MonacoStandaloneCodeEditor,
+} from '@materia-ui/ngx-monaco-editor';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { filter, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +14,77 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Front';
-  editorOptions = {theme: 'vs-dark', language: 'javascript'};
-  code: string= 'function x() {\nconsole.log("Hello world!");\n}';
+  @ViewChild(MonacoEditorComponent, { static: false })
+  monacoComponent: MonacoEditorComponent | undefined;
+  editorOptions: MonacoEditorConstructionOptions = {
+    theme: 'myCustomTheme',
+    language: 'html',
+    roundedSelection: true,
+    autoIndent: 'full'
+  };
+  code = this.getCode();
+  reactiveForm: FormGroup;
+  modelUri: monaco.Uri |undefined;
+
+  constructor(
+    private monacoLoaderService: MonacoEditorLoaderService,
+    private fb: FormBuilder
+  ) {
+    this.reactiveForm = this.fb.group({
+      code: [location]
+    });
+    this.monacoLoaderService.isMonacoLoaded$
+      .pipe(
+        filter(isLoaded => !!isLoaded),
+        take(1)
+      )
+      .subscribe(() => {
+        this.registerMonacoCustomTheme();
+      });
+  }
+
+  // mergeOptions(partialOptions) {
+  //   return {
+  //     ...this.editorOptions,
+  //     ...partialOptions
+  //   };
+  // }
+
+  editorInit(editor: MonacoStandaloneCodeEditor) {
+    // Programatic content selection example
+    editor.setSelection({
+      startLineNumber: 1,
+      startColumn: 1,
+      endColumn: 50,
+      endLineNumber: 3
+    });
+  }
+
+  getCode() {
+    return (
+      // tslint:disable-next-line: max-line-length
+      '<html><!-- // !!! Tokens can be inspected using F1 > Developer: Inspect Tokens !!! -->\n<head>\n	<!-- HTML comment -->\n	<style type="text/css">\n		/* CSS comment */\n	</style>\n	<script type="javascript">\n		// JavaScript comment\n	</' +
+      'script>\n</head>\n<body></body>\n</html>'
+    );
+  }
+
+  registerMonacoCustomTheme() {
+    monaco.editor.defineTheme('myCustomTheme', {
+      base: 'vs-dark', // can also be vs or hc-black
+      inherit: true, // can also be false to completely replace the builtin rules
+      rules: [
+        {
+          token: 'comment',
+          foreground: 'ffa500',
+          fontStyle: 'italic underline'
+        },
+        { token: 'comment.js', foreground: '008800', fontStyle: 'bold' },
+        { token: 'comment.css', foreground: '0000ff' } // will inherit fontStyle from `comment` above
+      ],
+      colors: {}
+    });
+  }
 }
+
+
 
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,19 +6,21 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { MonacoEditorModule } from 'ngx-monaco-editor';
+import { MonacoEditorModule, MONACO_PATH } from '@materia-ui/ngx-monaco-editor';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations:
+    [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    MonacoEditorModule.forRoot(),
+    MonacoEditorModule,
   ],
-  providers: [],
+  providers: [{
+    provide: MONACO_PATH,
+    useValue: 'https://unpkg.com/monaco-editor@0.24.0/min/vs'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
