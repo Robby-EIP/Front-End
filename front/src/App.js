b474ab87105ecb85648731f363bb1df2cb85c124
Lorenzo Manoeuvre <lorenzo.manoeuvre@epitech.eu>
2021-12-14 13:34:41 +0100

    switch to react

---
+++ front/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Editor from "@monaco-editor/react";
+
+function App() {
+  return (
+    <Editor
+     height="90vh"
+     defaultLanguage="javascript"
+     defaultValue="// some comment"
+   />
+
+  );
+}
+
+export default App;
